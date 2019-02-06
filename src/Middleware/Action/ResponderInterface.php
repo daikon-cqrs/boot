fde0fc28423ebe8c52e36725e5993d557d69bdff
Hasham Ahmad <mr.hash@gmail.com>
2019-02-06 20:36:05 +0000

    Add http status code interface

--- src/Middleware/Action/ResponderInterface.php
+++ src/Middleware/Action/ResponderInterface.php
@@ -4,10 +4,11 @@ declare(strict_types=1);
 
 namespace Oroshi\Core\Middleware\Action;
 
+use Fig\Http\Message\StatusCodeInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
-interface ResponderInterface
+interface ResponderInterface extends StatusCodeInterface
 {
     public function __invoke(ServerRequestInterface $request): ResponseInterface;
 }
