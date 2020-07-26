7c17cab9f27cea2d18fcd9c2d0f6069aa959fd40
Hasham Ahmad <mr.hash@gmail.com>
2020-07-26 15:16:26 -0500

    Remove erroneous dep

--- src/Middleware/Action/ErrorResponder.php
+++ src/Middleware/Action/ErrorResponder.php
@@ -10,7 +10,7 @@ namespace Daikon\Boot\Middleware\Action;
 
 use Daikon\Boot\Middleware\Action\Responder;
 use Daikon\Boot\Middleware\ActionHandler;
-use Laminas\Diactoros\Response\JsonResponse;
+use Middlewares\Utils\Factory;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
@@ -22,9 +22,12 @@ final class ErrorResponder extends Responder
         $statusCode = $request->getAttribute(ActionHandler::ATTR_STATUS_CODE, self::STATUS_INTERNAL_SERVER_ERROR);
         $errors = $request->getAttribute(ActionHandler::ATTR_ERRORS, []);
 
-        return new JsonResponse(
-            ['message' => $message] + (!empty($errors) ? ['errors' => $errors] : []),
-            $statusCode
-        );
+        return Factory::createResponse($statusCode)
+            ->withHeader('Content-Type', 'application/json')
+            ->withBody(Factory::createStream(json_encode(
+                ['message' => $message] + (!empty($errors) ? ['errors' => $errors] : []),
+                // taken from Laminas default encoding options
+                JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_AMP | JSON_HEX_QUOT | JSON_UNESCAPED_SLASHES
+            )));
     }
 }
